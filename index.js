8d61c80c12d4f7b4e5b572bf46f730f314d0c958
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-13 20:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+    
+    const {name, color, song, job, partner} = muppet;
 
 const k_muppet = {
   k_name: 'Kermit',
