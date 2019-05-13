0cc4b7a5ddd868b0b7db597a2ab97c460dd5a61c
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-13 20:30:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,4 +38,4 @@ const k_muppet = {
 
 const {name, color, song, job, partner} = muppet;
     
-    const {song_2, song_4, k_job, k_partner} =  k_muppet;
+    const {song_2, song_4, k_job, k_partner} =  k_muppet.k_album.the_muppet_movie;
