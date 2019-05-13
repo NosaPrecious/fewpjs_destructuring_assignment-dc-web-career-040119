ad33f167196f344135fd8656725e9384cac8dd3c
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-13 20:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const muppet = {
   partner: 'Miss Piggy'
 };
 
-const {name, color, song, job, partner} = muppet;
-
 const k_muppet = {
   k_name: 'Kermit',
   k_color: 'green',
@@ -37,28 +35,4 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
-const {
-
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
-}
+const {name, color, song, job, partner} = muppet;
