65c93ff2679112df6868d4d80c70eb1793a69057
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-13 20:22:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const muppet = {
   partner: 'Miss Piggy'
 };
     
-    const {name, color, song, job, partner} = muppet;
+const {name, color, song, job, partner} = muppet;
 
 const k_muppet = {
   k_name: 'Kermit',
